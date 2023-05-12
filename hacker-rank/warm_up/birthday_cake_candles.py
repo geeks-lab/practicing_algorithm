f0a5719dde1e1aff6cb28e9a9df2391a1222ca7a
gigis-lab <churn82@gmail.com>
2023-05-12 18:05:55 +0900

    birthdayCakeCandles

--- hacker-rank/warm_up/birthday_cake_candles.py
+++ hacker-rank/warm_up/birthday_cake_candles.py
@@ -1,8 +1,21 @@
 
+'''
+Sample Input 
 
-def birthdayCakeCandles(candles):
-    
-    print(candles)
-    
+4
+3 2 1 3
 
-birthdayCakeCandles(input())
+Sample Output 
+
+2
+'''
+
+
+def birthdayCakeCandles():
+        
+    candle_nums = int(input())
+    candles = list(map(int,input().split()))
+    print(candles.count(max(candles)))
+        
+
+birthdayCakeCandles()
