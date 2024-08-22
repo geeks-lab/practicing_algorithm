8bd2f80e2087e0f4f01cf2d8da9852b91bb69304
geeks-lab <churn82@gmail.com>
2024-08-22 20:51:37 +0900

    n queen problem

--- backjoon/0820_15649_N과M.py
+++ backjoon/0820_15649_N과M.py
@@ -1,19 +1,16 @@
+
 def dfs():
     if len(s) == m:
         print(' '.join(map(str, s)))
         return
     for i in range(1, n+1):
-        if visited[i]:
-            continue
-        visited[i] = True
-        s.append(i)
-        dfs()
-        s.pop()
-        visited[i] = False
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
             
 
 n, m = map(int, input().split())
 s = []
-visited = [False] * (n+1)
 
 dfs()
